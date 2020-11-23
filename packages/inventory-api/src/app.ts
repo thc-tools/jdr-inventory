168aaed525ebbcb4844cc3ea7b8bfa6be13da5cb
c3dr0x <cedric.boulet-kessler@dreamquark.com>
2020-11-23 22:36:54 +0100

    feat: add pino as logger

--- packages/inventory-api/src/app.ts
+++ packages/inventory-api/src/app.ts
@@ -3,12 +3,14 @@ import { ApolloServer } from "apollo-server";
 
 // Infrastructure
 import { getConnection } from "./infrastructure/db";
+import { logger } from "./infrastructure/logger";
 
 // Graph
 import { schema, resolvers } from "./graph";
 import { createDataSources } from "./datasources";
 
 export const server = new ApolloServer({
+    logger,
     resolvers,
     typeDefs: schema,
     dataSources: () => {
--- packages/inventory-api/src/graph/resolvers/_utils.ts
+++ packages/inventory-api/src/graph/resolvers/_utils.ts
@@ -1,16 +1,16 @@
-export function responsify<P, R = P>(
-    promise: () => Promise<P>,
-    success: (p: P) => R
-): Promise<{ error?: string } | R> {
+// Infrastructure
+import { logger } from "../../infrastructure/logger";
+
+export function responsify<P, R = P>(promise: () => Promise<P>, success: (p: P) => R): Promise<{ error?: string } | R> {
     try {
         return promise()
             .then(success)
             .catch((err: Error) => {
-                console.error(err);
+                logger.error(err);
                 return { error: err.message };
             });
     } catch (err) {
-        console.error(err);
+        logger.error(err);
         return Promise.resolve({ error: err.message });
     }
 }
